08d8584a55533760e43cb7ef1d1f75b90269f04b
Adetunji Femi <phemmzmcllroy@gmail.com>
2019-10-20 03:03:36 +0100

    Setup react navigation and vector icons

--- App.js
+++ App.js
@@ -1,49 +1,98 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator, BottomTabBar } from 'react-navigation-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  LoginScreen, CreateAccountScreen, AddCardScreen
+} from './src/components/Auth';
+import { HomeScreen } from './src/components/Home';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const TabBarComponent = props => <BottomTabBar {...props} />;
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+const AppStack = createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) =>
+          <MaterialCommunityIcons name="home" size={25} color={focused ? "#030404" : "#8F9FB3"} />
+      }
+    },
+    Statement: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) =>
+          <AntDesign name="filetext1" size={25} color={focused ? "#030404" : "#8F9FB3"} />
+      }
+    },
+    Add: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarIcon: () =>
+          <Ionicons style={{ marginBottom: 10 }} name="ios-add-circle" size={40} color="#4EAC50" />,
+        tabBarLabel: () => null
+      }
+    },
+    CashOut: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) =>
+          <MaterialCommunityIcons name="currency-ngn" size={25} color={focused ? "#030404" : "#8F9FB3"} />
+      }
+    },
+    Profile: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) =>
+          <AntDesign name="user" size={25} color={focused ? "#030404" : "#8F9FB3"} />
+      }
+    },
+  },
+  {
+    tabBarComponent: props => <TabBarComponent {...props} />,
+    tabBarOptions: {
+      style: {
+        elevation: 0,
+        shadowOpacity: 0,
+        borderTopWidth: 0,
+      },
+      activeTintColor: '#030404',
+      inactiveTintColor: '#8F9FB3'
   }
-}
+  }
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+const AuthStack = createStackNavigator(
+  {
+    Login: {
+      screen: LoginScreen,
+    },
+    CreateAccount: {
+      screen: CreateAccountScreen,
+    },
+    AddCard: {
+      screen: AddCardScreen,
+    },
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  {
+    headerMode: 'none',
+    initialRouteName: 'Login'
+  }
+);
+
+const AppNavigator = createSwitchNavigator(
+  {
+    AuthStack: AuthStack,
+    AppStack: AppStack
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  {
+    headerMode: 'none',
+    initialRouteName: 'AuthStack',
+  }
+);
+
+export default createAppContainer(AppNavigator);
